406f4e317bcdc0d66445da5d9df04dae1e227144
Emile Bergeron <ber.emile@gmail.com>
2021-12-14 14:17:56 -0500

    Adds theme and nvm plugins installation script

--- install.sh
+++ install.sh
@@ -1,3 +1,7 @@
 
+git clone https://github.com/bhilburn/powerlevel9k.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes/powerlevel9k
+
 # installs zsh-syntax-highlighting plugin to a common directory
 git clone git@github.com:zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
+git clone https://github.com/lukechilds/zsh-nvm ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-nvm
