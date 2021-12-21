8a52b73bc5e7a40e93491938732fe128ac677a24
Emile Bergeron <ber.emile@gmail.com>
2021-12-21 18:24:01 -0500

    Fixes install script

--- install.sh
+++ install.sh
@@ -3,15 +3,15 @@
 set -e
 
 DOTFILES_ROOT=$(pwd -P)
-echo "Dotfiles dir: $DOTFILES_ROOT"
+echo -e "Dotfiles dir: $DOTFILES_ROOT"
 
 OMZ_DIR=$HOME/.oh-my-zsh
-echo "Oh-My-ZSH path: $OMZ_DIR"
+echo -e "Oh-My-ZSH path: $OMZ_DIR"
 
 [ ! -d "$OMZ_DIR" ] && sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 ZSH_CUSTOM=${ZSH_CUSTOM:-$OMZ_DIR/custom}
-echo "ZSH_CUSTOM path: $ZSH_CUSTOM"
+echo -e "ZSH_CUSTOM path: $ZSH_CUSTOM"
 
 ZSH_THEMES=${ZSH_CUSTOM}/themes
 ZSH_PLUGINS=${ZSH_CUSTOM}/plugins
@@ -19,7 +19,7 @@ ZSH_PLUGINS=${ZSH_CUSTOM}/plugins
 [ ! -d "$ZSH_THEMES/powerlevel9k" ] && git clone https://github.com/bhilburn/powerlevel9k.git ${ZSH_THEMES}/powerlevel9k
 
 # installs zsh-syntax-highlighting plugin to a common directory
-[ ! -d "$ZSH_PLUGINS/sh-syntax-highlighting" ] && git clone git@github.com:zsh-users/zsh-syntax-highlighting.git ${ZSH_PLUGINS}/zsh-syntax-highlighting
+[ ! -d "$ZSH_PLUGINS/zsh-syntax-highlighting" ] && git clone git@github.com:zsh-users/zsh-syntax-highlighting.git ${ZSH_PLUGINS}/zsh-syntax-highlighting
 [ ! -d "$ZSH_PLUGINS/zsh-nvm" ] && git clone https://github.com/lukechilds/zsh-nvm ${ZSH_PLUGINS}/zsh-nvm
 
 # dotfiles
@@ -27,3 +27,5 @@ ln -sf ${DOTFILES_ROOT}/zshrc ~/.zshrc
 ln -sf ${DOTFILES_ROOT}/gitconfig ~/.gitconfig
 ln -sf ${DOTFILES_ROOT}/gitignore ~/.gitignore
 ln -sf ${DOTFILES_ROOT}/aliases ~/.aliases
+
+echo -e "Dotfiles installed succesfully!"
